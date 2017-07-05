099ffc2bef8d6959d91ecab582d1bd397fb5e0e5
terrysoltz <terrence.soltz@gmail.com>
2017-07-05 13:26:21 -0700

    Add project files.

---
+++ MyLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyLibrary.Startup))]
+namespace MyLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
